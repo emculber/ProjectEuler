e63f8e6532af795f4443867e1e3352b5d5830398
Erik Culberson <eculberson12@gmail.com>
2015-03-09 19:17:40 -0400

    added timer to Problem 505

--- Problem505.java
+++ Problem505.java
@@ -6,64 +6,214 @@ public class Problem505
     new Problem505();
   }
 
+  long opened = 0;
+  long closed = 0;
+  long startTime = 0;
+  double hour = 0;
+  double min = 0;
+  double second = 0;
+  double millisecond = 0;
+
   public Problem505() {
-    System.out.println("x(2)    : " + x(2));
-    /*
-    System.out.println("x(3)    : " + x(3));
-    System.out.println("x(4)    : " + x(4));
-    System.out.println("y4(4)   : " + y(4,4));
-    System.out.println("y4(3)   : " + y(4,3));
-    System.out.println("y4(2)   : " + y(4,2));
-    System.out.println("y4(1)   : " + y(4,1));
-    System.out.println("A(10)   : " + A(10));
-    System.out.println("A(10^3) : " + A(Math.pow(10, 3)));
-    System.out.println("A(10^12): " + A(Math.pow(10, 12)));*/
+    startTime = System.currentTimeMillis();
+    double test = x(2);
+    System.out.println("\nx(2)     : " + test);
+
+    opened = closed = 0;
+    startTime = System.currentTimeMillis();
+    test = x(3);
+    System.out.println("\nx(3)     : " + test);
+    
+    opened = closed = 0;
+    startTime = System.currentTimeMillis();
+    test = x(4);
+    System.out.println("\nx(4)     : " + test);
+    
+    opened = closed = 0;
+    startTime = System.currentTimeMillis();
+    test = y(4,4);
+    System.out.println("\ny4(4)    : " + test);
+    
+    opened = closed = 0;
+    startTime = System.currentTimeMillis();
+    test = y(4,3) - (2<<60);
+    System.out.println("\ny4(3)    : " + test);
+    
+    opened = closed = 0;
+    startTime = System.currentTimeMillis();
+    test = y(4,2) - (2<<60);
+    System.out.println("\ny4(2)    : " + test);
+    
+    opened = closed = 0;
+    startTime = System.currentTimeMillis();
+    test = y(4,1);
+    System.out.println("\ny4(1)    : " + test);
+    
+    opened = closed = 0;
+    startTime = System.currentTimeMillis();
+    test = A(10) - (2<<60);
+    System.out.println("\nA(10)    : 2^60" + test);
+    
+    opened = closed = 0;
+    startTime = System.currentTimeMillis();
+    test = A(Math.pow(10, 3));
+    System.out.println("\nA(10^3)  : " + test); 
+    
+    opened = closed = 0;
+    startTime = System.currentTimeMillis();
+    test = A(Math.pow(10, 4));
+    System.out.println("\nA(10^4)   : " + test);
+    
+    opened = closed = 0;
+    startTime = System.currentTimeMillis();
+    test = A(Math.pow(10, 5));
+    System.out.println("\nA(10^5)   : " + test);
+    
+    opened = closed = 0;
+    startTime = System.currentTimeMillis();
+    test = A(Math.pow(10, 6));
+    System.out.println("\nA(10^6)   : " + test);
+    
+    opened = closed = 0;
+    startTime = System.currentTimeMillis();
+    test = A(Math.pow(10, 7));
+    System.out.println("\nA(10^7)   : " + test);
+    
+    opened = closed = 0;
+    startTime = System.currentTimeMillis();
+    test = A(Math.pow(10, 8));
+    System.out.println("\nA(10^8)   : " + test);
+    
+    opened = closed = 0;
+    startTime = System.currentTimeMillis();
+    test = A(Math.pow(10, 9));
+    System.out.println("\nA(10^9)   : " + test);
+    
+    opened = closed = 0;
+    startTime = System.currentTimeMillis();
+    test = A(Math.pow(10, 10));
+    System.out.println("\nA(10^10)   : " + test);
+    
+    opened = closed = 0;
+    startTime = System.currentTimeMillis();
+    test = A(Math.pow(10, 11));
+    System.out.println("\nA(10^11)   : " + test);
+    
+    opened = closed = 0;
+    startTime = System.currentTimeMillis();
+    test = A(Math.pow(10, 12));
+    System.out.println("\nA(10^12)   : " + test);
+    
+    long estimatedTime = System.currentTimeMillis() - startTime;
+    System.out.println("\nElapsedTime : " + estimatedTime);
   }
 
   private double x(double f) {
-    System.out.println("Running x(" + f + ")");
-    if(f == 0) {
-      System.out.println("x1)Return " + f);
-      return f;
-    }
-    if(f == 1) {
-      System.out.println("x2)Return " + f);
+    opened++;
+    millisecond = System.currentTimeMillis() - startTime;
+    second = Math.floor(millisecond / 1000);
+    min = Math.floor(second / 60);
+    hour = Math.floor(min / 60);
+    System.out.print("Opened=" + opened + " :: Closed=" + closed + " :: Unclosed=" + (opened - closed) + " :: Time=" + hour + ":" + min + ":" + second + ":" + millisecond  + "                  \r");
+    if(f == 0 || f == 1) {
+      closed++;
+    millisecond = System.currentTimeMillis() - startTime;
+    second = Math.floor(millisecond / 1000);
+    min = Math.floor(second / 60);
+    hour = Math.floor(min / 60);
+      System.out.print("Opened=" + opened + " :: Closed=" + closed + " :: Unclosed=" + (opened - closed) + " :: Time=" + hour + ":" + min + ":" + second + ":" + millisecond  + "                  \r");
       return f;
     }
     if(f % 2 == 0 && f >= 1) {
       double k = f/2;
-      double temp = (3 * x(k) + 2 * x(Math.floor(k/2)) % Math.pow(2, 60));
-      System.out.println("x3)Return " + temp);
+      double floorHalfk = Math.floor(k/2);
+      double part1 = 3 * x(k);
+      double part2 = 2 * x(floorHalfk);
+      double part3 = part1 + part2;
+      double temp = part3 % (2 << 60);
+      closed++;
+    millisecond = System.currentTimeMillis() - startTime;
+    second = Math.floor(millisecond / 1000);
+    min = Math.floor(second / 60);
+    hour = Math.floor(min / 60);
+      System.out.print("Opened=" + opened + " :: Closed=" + closed + " :: Unclosed=" + (opened - closed) + " :: Time=" + hour + ":" + min + ":" + second + ":" + millisecond  + "                  \r");
       return temp;
     }
     else if (f >= 1) {
-      double k = (f/2 - 1);
-      double temp = (2 * x(k) + 3 * x(Math.floor(k/2)) % Math.pow(2, 60));
-      System.out.println("x4)Return " + temp);
+      double k = ((f-1)/2);
+      double floorHalfk = Math.floor(k/2);
+      double part1 = 2 * x(k);
+      double part2 = 3 * x(floorHalfk);
+      double part3 = part1 + part2;
+      double temp = part3 % (2 << 60);
+      closed++;
+    millisecond = System.currentTimeMillis() - startTime;
+    second = Math.floor(millisecond / 1000);
+    min = Math.floor(second / 60);
+    hour = Math.floor(min / 60);
+      System.out.print("Opened=" + opened + " :: Closed=" + closed + " :: Unclosed=" + (opened - closed) + " :: Time=" + hour + ":" + min + ":" + second + ":" + millisecond  + "                  \r");
       return temp;
     }
-    System.out.println("Should never Reach this point. Return " + f);
-    return f;
+    closed++;
+    millisecond = System.currentTimeMillis() - startTime;
+    second = Math.floor(millisecond / 1000);
+    min = Math.floor(second / 60);
+    hour = Math.floor(min / 60);
+    System.out.print("Opened=" + opened + " :: Closed=" + closed + " :: Unclosed=" + (opened - closed) + " :: Time=" + hour + ":" + min + ":" + second + ":" + millisecond  + "                  \r");
+    return 0;
   }
 
   private double y(double n, double k) {
-    System.out.println("Running y" + n + "("+k+")");
+    opened++;
+    millisecond = System.currentTimeMillis() - startTime;
+    second = Math.floor(millisecond / 1000);
+    min = Math.floor(second / 60);
+    hour = Math.floor(min / 60);
+    System.out.print("Opened=" + opened + " :: Closed=" + closed + " :: Unclosed=" + (opened - closed) + " :: Time=" + hour + ":" + min + ":" + second + ":" + millisecond  + "                  \r");
     if(k >= n) {
       double temp = x(k);
-      System.out.println("1y)Return " + temp);
+      closed++;
+    millisecond = System.currentTimeMillis() - startTime;
+    second = Math.floor(millisecond / 1000);
+    min = Math.floor(second / 60);
+    hour = Math.floor(min / 60);
+      System.out.print("Opened=" + opened + " :: Closed=" + closed + " :: Unclosed=" + (opened - closed) + " :: Time=" + hour + ":" + min + ":" + second + ":" + millisecond  + "                  \r");
       return x(k);
     }
     if(k < n) {
-      double temp = Math.pow(2, 60) - 1 - Math.max(y(n, 2*k), y(n, (2*k + 1)));
-      System.out.println("2y)Return " + temp);
+      double max = Math.max(y(n, 2*k), y(n, (2*k + 1)));
+      double temp = (2<<60) - 1 - max;
+      closed++;
+    millisecond = System.currentTimeMillis() - startTime;
+    second = Math.floor(millisecond / 1000);
+    min = Math.floor(second / 60);
+    hour = Math.floor(min / 60);
+      System.out.print("Opened=" + opened + " :: Closed=" + closed + " :: Unclosed=" + (opened - closed) + " :: Time=" + hour + ":" + min + ":" + second + ":" + millisecond  + "                  \r");
       return temp;
     }
-    System.out.println("Should never Reach this point. Return " + 0);
+    closed++;
+    millisecond = System.currentTimeMillis() - startTime;
+    second = Math.floor(millisecond / 1000);
+    min = Math.floor(second / 60);
+    hour = Math.floor(min / 60);
+    System.out.print("Opened=" + opened + " :: Closed=" + closed + " :: Unclosed=" + (opened - closed) + " :: Time=" + hour + ":" + min + ":" + second + ":" + millisecond  + "                  \r");
     return 0;
   }
 
   private double A(double n){
-    System.out.println("Running A(" + n + ")");
-    return y(n, 1);
+    opened++;
+    millisecond = System.currentTimeMillis() - startTime;
+    second = Math.floor(millisecond / 1000);
+    min = Math.floor(second / 60);
+    hour = Math.floor(min / 60);
+    System.out.print("Opened=" + opened + " :: Closed=" + closed + " :: Unclosed=" + (opened - closed) + " :: Time=" + hour + ":" + min + ":" + second + ":" + millisecond  + "                  \r");
+    double temp = y(n, 1);
+    closed++;
+    millisecond = System.currentTimeMillis() - startTime;
+    second = Math.floor(millisecond / 1000);
+    min = Math.floor(second / 60);
+    hour = Math.floor(min / 60);
+    System.out.print("Opened=" + opened + " :: Closed=" + closed + " :: Unclosed=" + (opened - closed) + " :: Time=" + hour + ":" + min + ":" + second + ":" + millisecond  + "                  \r");
+    return temp;
   }
 }
